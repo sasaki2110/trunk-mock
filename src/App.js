f02dff2fd42ccabd4c8da5af210cdc4b78476b4c
sasaki2110 <sasaki2110@gmail.com>
2023-12-06 18:37:55 +0900

    response karitaiou

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "@aws-amplify/ui-react/styles.css";
 import { 
   Flex, 
@@ -12,13 +13,15 @@ import {
   TableBody,
   TableHead,
   TableRow,
+  ToggleButton,
+  useBreakpointValue,
 } from '@aws-amplify/ui-react';
 
 // メニューバー
 // Gridで５フレームを横方向に用意
 // 何も指定せずにイメージを置いたら１フレーム目に入った
 // 残りを右寄せのFlexとして、２フレーム目から配置。なぜエンドは６？？？
-function MenuBar() {
+function MenuBar1() {
   return(
     <Grid templateColumns="1fr 1fr 1fr 1fr 1fr">
       <Image 
@@ -41,49 +44,76 @@ function MenuBar() {
   )
 }
 
-function BackUp() {
-  return (
-    <View>
-      <Flex direction="column" maxWidth="90%" margin="0 auto">
-        <MenuBar/>
-          <h1>トランクルーム掘げほげ</h1>
-          <Flex gap="0rem" direction="column">
-            <Label height="1rem">◆ここに店舗説明</Label>
-            <Label height="1rem">◆ここに店舗説明がならぶはず</Label>
-          </Flex>
-          <Label>東京都品川区東品川１丁目</Label>
+function MenuBar2() {
+  const [isOpen, setIsOpen] = useState(false);
+  function handleClick(){
+    setIsOpen(!isOpen);
+  }
+
+  function ToggleMenu(){
+    if(!isOpen){return}
+    return(
+      <Flex 
+        justifyContent="flex-end"
+        columnStart="1"
+        columnEnd="6"
+        direction="column"
+      >
+        <Button border="none">店舗情報</Button>
+        <Button border="none">ご利用ガイド</Button>
+        <Button border="none">よくあるご質問</Button>
+        <Button border="none">お問い合わせ・見学予約</Button>
+        <Button border="none">ログイン</Button>
       </Flex>
-      <Flex direction="row" maxWidth="90%" margin="0 auto">
-        <img src="img/sinagawa1.jpeg" alt="ほんとはピロピロしたい" height="450px"/>
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3241.4012566583356!2d139.80864417476812!3d35.667120669996976!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6018890082b85bc3%3A0xcf533b02ac169ede!2z44Ki44OR44Ob44OG44Or44CI5p2x5Lqs5pyo5aC044CJ!5e0!3m2!1sja!2sjp!4v1701843061344!5m2!1sja!2sjp" width="600" height="450" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    )
+  }
+  return(
+    <Grid templateColumns="1fr 1fr 1fr 1fr 1fr">
+      <Image 
+        src="logo192.png"
+        height="5em"
+      />
+
+      <Flex
+        justifyContent="flex-end"
+        columnStart="2"
+        columnEnd="6"
+      >
+        <ToggleButton onClick={handleClick}>三</ToggleButton>
       </Flex>
-      <Grid templateColumns="1fr 1fr" maxWidth="90%" margin="0 auto">
-        <Flex>
-          <Label>ここはほんとはスワイパーを置きたい</Label>
-        </Flex>
-        <Flex direction="column">
-          <Label>住所やら</Label>
-          <Label>電話番号やら</Label>
-        </Flex>
-      </Grid>
-    </View>
-  );
+      <ToggleMenu />
+    </Grid>
+  )
+
+}
+
+function MenuBar( { breakPoint } ) {
+  if(breakPoint=="small") {
+    return(MenuBar2());
+  } else {
+    return(MenuBar1());
+  }
 }
 
 export default function App() {
   const { tokens } = useTheme();
+  const breakPoint = useBreakpointValue( {
+    small: "small",
+    medium: "medium",
+    xxl: "xxl",
+  })
 
   return (
     <View>
       <Flex direction="column" maxWidth="90%" margin="0 auto">
-        <MenuBar/>
+        <MenuBar breakPoint={breakPoint}/>
           <h1><font  color={tokens.colors.red[40]}>トランクルーム掘げほげ</font></h1>
           <Flex gap="0rem" direction="column">
             <Label height="1rem">◆ここに店舗説明</Label>
             <Label height="1rem">◆ここに店舗説明がならぶはず</Label>
           </Flex>
           <Label>東京都品川区東品川１丁目</Label>
-          <Flex direction="row">
+          <Flex direction="row" wrap="wrap">
             <Flex direction="column">
               <img src="img/sinagawa1.png" alt="ほんとはピロピロしたい" height="450px"/>
               <Label>ここはほんとはスワイパーを置きたい</Label>
@@ -94,6 +124,7 @@ export default function App() {
               <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3241.4012566583356!2d139.80864417476812!3d35.667120669996976!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6018890082b85bc3%3A0xcf533b02ac169ede!2z44Ki44OR44Ob44OG44Or44CI5p2x5Lqs5pyo5aC044CJ!5e0!3m2!1sja!2sjp!4v1701843061344!5m2!1sja!2sjp" width="600" height="450" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
               <Label>住所やら</Label>
               <Label>電話番号やら</Label>
+              <Label>駐車場やら</Label>
             </Flex>
           </Flex>
           <Flex direction="column" gap="0rem">
