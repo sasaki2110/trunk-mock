e258925d5ed739f1b1e4d97b378e586114eb49f3
sasaki2110 <sasaki2110@gmail.com>
2023-12-06 10:59:51 +0900

    added auth

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import { Amplify } from 'aws-amplify';
+import config from './aws-exports';
+import "@aws-amplify/ui-react/styles.css";
+import { Grid, View, useTheme } from '@aws-amplify/ui-react';
+
+Amplify.configure(config);
 
 function App() {
+  const { tokens } = useTheme();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React v2
-        </a>
-      </header>
-    </div>
+    <Grid
+      templateColumns="1fr 1fr"
+      templateRows="10rem 10rem"
+      gap={tokens.space.small}
+    >
+      <View backgroundColor={tokens.colors.blue[10]}></View>
+      <View backgroundColor={tokens.colors.blue[20]}></View>
+      <View backgroundColor={tokens.colors.blue[40]}></View>
+      <View backgroundColor={tokens.colors.blue[60]}></View>
+    </Grid>
   );
 }
 
