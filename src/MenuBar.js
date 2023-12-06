85e1f69bcd7c228ec4cbf6fcd4efacf27c94484e
sasaki2110 <sasaki2110@gmail.com>
2023-12-06 22:01:19 +0900

    gamen senni mae ni ittan commit

---
+++ src/MenuBar.js
@@ -0,0 +1,97 @@
+import { useState } from "react";
+import "@aws-amplify/ui-react/styles.css";
+import { 
+  Flex, 
+  Button, 
+  Image,
+  Grid, 
+  ToggleButton,
+  Link,
+} from '@aws-amplify/ui-react';
+
+function Buttons({direction}) {
+  return(
+    <Flex direction={direction}>
+      <Button border="none">店舗情報</Button>
+      <Button border="none">ご利用ガイド</Button>
+      <Button border="none">よくあるご質問</Button>
+      <Button border="none">お問い合わせ・見学予約</Button>
+      <Button border="none">ログイン</Button>
+    </Flex>
+)
+}
+
+// メニューバー１（PC用）
+// Gridで５フレームを横方向に用意
+// 何も指定せずにイメージを置いたら１フレーム目に入った
+// 残りを右寄せのFlexとして、２フレーム目から配置。なぜエンドは６？？？
+function MenuBar1() {
+  return(
+    <Grid templateColumns="1fr 1fr 1fr 1fr 1fr">
+      <Image 
+        src="logo192.png"
+        height="5em"
+      />
+
+      <Flex
+        justifyContent="flex-end"
+        columnStart="2"
+        columnEnd="6"
+      >
+        <Buttons direction="row"/>
+      </Flex>
+    </Grid>
+  )
+}
+
+// メニューバー２（スマホ用）
+// 「三」ボタンでメニューバーとトグルする
+function MenuBar2() {
+  const [isOpen, setIsOpen] = useState(false);
+  function handleClick(){
+    setIsOpen(!isOpen);
+  }
+
+  function ToggleMenu(){
+    if(!isOpen){return}
+    return(
+      <Flex 
+        justifyContent="flex-end"
+        columnStart="1"
+        columnEnd="6"
+        direction="column"
+      >
+        <Buttons direction="column"/>
+      </Flex>
+    )
+  }
+
+  return(
+    <Grid templateColumns="1fr 1fr 1fr 1fr 1fr">
+      <Image 
+        src="logo192.png"
+        height="5em"
+      />
+
+      <Flex
+        justifyContent="flex-end"
+        columnStart="2"
+        columnEnd="6"
+      >
+        <ToggleButton onClick={handleClick}>三</ToggleButton>
+      </Flex>
+      <ToggleMenu />
+    </Grid>
+  )
+}
+
+// 実際に画面から使用するメニューバー
+// 画面サイズ（ブレークポイント）によって、１か２を切り替える
+// ここは適当だから、本当に網羅できているか解らない
+export default function MenuBar( { breakPoint } ) {
+  if(breakPoint==="small" || breakPoint===null)  {
+    return(MenuBar2());
+  } else {
+    return(MenuBar1());
+  }
+}
---
+++ src/Reserv.js
@@ -0,0 +1,13 @@
+import "@aws-amplify/ui-react/styles.css";
+import { 
+    Label,
+    View
+} from '@aws-amplify/ui-react';
+  
+export default function Reserve() {
+    return(
+        <View>
+            <Label>RESERVATION</Label>
+        </View>
+    )
+}
